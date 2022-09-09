57f5697434d2b3c6ef9788060f8a9ac44ace374b
erenkucuk <erenkkucuk89@gmail.com>
2022-09-09 03:23:05 -1000

    added new lines

--- src/test/java/DAY1/JDBC_FirstStep.java
+++ src/test/java/DAY1/JDBC_FirstStep.java
@@ -1,18 +1,10 @@
+package DAY1;
+
 import java.sql.*;
 
-public class TestConnection {
+public class JDBC_FirstStep {
 
     public static void main(String[] args) {
-
-        /*
-        host : 3.91.182.164
-        port: 1521
-        SID : xe
-        user : hr
-        pass: hr
-
-        jdbc:oracle:thin:@3.91.182.164
-         */
 
         String url = "jdbc:oracle:thin:@3.91.182.164:1521:XE";
         String username = "hr";
