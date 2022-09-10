f3e80cc8bcd8daae227b6f37e42c16b7216a1bcc
erenkucuk <erenkkucuk89@gmail.com>
2022-09-10 01:29:13 -1000

    added new lines

--- src/test/java/DAY1/ResultSetMetaData_ForColumnInfo.java
+++ src/test/java/DAY1/ResultSetMetaData_ForColumnInfo.java
@@ -1,4 +1,19 @@
 package DAY1;
 
+import java.sql.*;
+
 public class ResultSetMetaData_ForColumnInfo {
+
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:oracle:thin:@3.91.182.164:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        Connection con = DriverManager.getConnection(url, username, password);
+        Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet rs = stm.executeQuery("SELECT * FROM REGIONS");
+
+
+    }
 }
