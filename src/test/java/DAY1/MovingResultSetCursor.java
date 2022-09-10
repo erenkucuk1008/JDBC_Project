68bc4e89836ce8a911e8b655aa49aadd37306e2c
erenkucuk <erenkkucuk89@gmail.com>
2022-09-10 00:45:36 -1000

    added new lines

--- src/test/java/DAY1/MovingResultSetCursor.java
+++ src/test/java/DAY1/MovingResultSetCursor.java
@@ -13,6 +13,46 @@ public class MovingResultSetCursor {
         Connection con = DriverManager.getConnection(url, username, password);
         Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet rs = stm.executeQuery("SELECT * FROM REGIONS");
+
+        /*
+        These are the method of resultSet obj we can use to move the cursor accordingly
+        This will only work if your result is type scroll insensitive
+        rs.next();
+        rs.previous();
+        rs.first();
+        rs.last();
+        rs.beforeFirst();
+        rs.afterLast();
+        rs.absolute(3);
+         */
+
+        rs.first();
+        System.out.println("First row : "+rs.getString(2));
+        rs.last();
+        System.out.println("Last row : "+rs.getString(2));
+        rs.previous();
+        System.out.println("Previous row : "+rs.getString(2));
+
+        rs.beforeFirst();
+        while (rs.next()){
+            System.out.println("2nd column data: "+ rs.getString(2));
+        }
+
+        //butun row lara bakiyor, daha fazla row kalmadigi icin
+        //loop dan cikiyor. bundan sonra yine soutla rs.getstring yaparsak hata verir
+        //cunku row kalmadi. en son row a kadar loop ta baktik
+        //System.out.println("2nd row is: "+rs.getString(2));
+
+
+        //bu asamada 2.row a gelelim.
+        //move to 2nd row from this point and print region_name
+
+        rs.absolute(2);
+        System.out.println("2nd row is: "+ rs.getString(2));
+
+
+
+
     }
 }
 
