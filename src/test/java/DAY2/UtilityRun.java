3dfd3968d136ebd4bc17acbce6ec86a18382829d
erenkucuk <erenkkucuk89@gmail.com>
2022-09-11 03:59:36 -1000

    added new lines

--- src/test/java/DAY2/UtilityRun.java
+++ src/test/java/DAY2/UtilityRun.java
@@ -11,17 +11,10 @@ public class UtilityRun {
     public static void main(String[] args) throws SQLException {
 
         DB_Utility.createConnection();
-
-
-        String url = ConfigurationReader.getProperty("hr.database.url");
-        String username = ConfigurationReader.getProperty("hr.database.username");
-        String password = ConfigurationReader.getProperty("hr.database.password");
-
-        DB_Utility.createConnection(url, username, password);
-
-        ResultSet result = DB_Utility.runQuery("SELECT * FROM REGIONS");
-        result.next();
-        System.out.println("REGION_NAME= " + result.getString("REGION_NAME"));
+        ResultSet result = DB_Utility.runQuery("SELECT * FROM EMPLOYEES");
+        DB_Utility.getAllColumnNames();
+        DB_Utility.getAllRows();
+        DB_Utility.destroy();
 
     }
 }
--- src/test/java/utility/DB_Utility.java
+++ src/test/java/utility/DB_Utility.java
@@ -1,12 +1,16 @@
 package utility;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DB_Utility {
 
     private static Connection con;
     private static Statement stm;
     private static ResultSet rs;
+    private static ResultSetMetaData rsmd;
+
 
     public static void createConnection() {
 
@@ -15,31 +19,92 @@ public class DB_Utility {
         String password = ConfigurationReader.getProperty("hr.database.password");
 
         try {
-            con = DriverManager.getConnection(url,username,password);
+            con = DriverManager.getConnection(url, username, password);
             System.out.println("Connection was successfull");
-        }catch (SQLException e){
-            System.out.println("Connection has failed "+ e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Connection has failed " + e.getMessage());
         }
     }
 
-    public static void createConnection(String url, String username, String password){
+    /**
+     * @param url
+     * @param username
+     * @param password
+     */
+    public static void createConnection(String url, String username, String password) {
+
         try {
-            con = DriverManager.getConnection(url,username,password);
+            con = DriverManager.getConnection(url, username, password);
             System.out.println("Connection was successfull");
-        }catch (SQLException e){
-            System.out.println("Connection has failed "+ e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Connection has failed " + e.getMessage());
         }
     }
 
-    public static ResultSet runQuery(String sql){
+    /**
+     * Run the sql query provided and return ResultSet object
+     *
+     * @param sql
+     * @return ResultSet object that contains data
+     */
+    public static ResultSet runQuery(String sql) {
 
         try {
             stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             rs = stm.executeQuery(sql);
-        } catch (SQLException e){
-            System.out.println("Error Occured While Running Query "+e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error Occured While Running Query " + e.getMessage());
         }
 
         return rs;
     }
+
+    public static ResultSet getAllColumnNames() {
+
+        try {
+            rsmd = rs.getMetaData();
+            int colCount = rsmd.getColumnCount();
+            for (int col = 1; col <= colCount; col++) {
+                System.out.print(rsmd.getColumnName(col) + "\t");
+            }
+            System.out.println();
+        } catch (SQLException e) {
+            System.out.println("Error occured while getting column names " + e.getMessage());
+        }
+        return rs;
+    }
+
+    public static ResultSet getAllRows() {
+
+        try {
+            rs.beforeFirst();
+            rsmd = rs.getMetaData();
+            int colCount = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int col = 1; col <= colCount; col++) {
+                    System.out.print(rs.getString(col) + "\t");
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error occured while getting all rows " + e.getMessage());
+        }
+
+        return rs;
+    }
+
+    /**
+     * destroy method to clean up all the resources after being used
+     */
+    public static void destroy() {
+
+        try {
+            if (rs != null) rs.close();
+            if (stm != null) stm.close();
+            if (con != null) con.close();
+        } catch (SQLException e) {
+            System.out.println("Error occured while closing resources " + e.getMessage());
+        }
+    }
+
 }
