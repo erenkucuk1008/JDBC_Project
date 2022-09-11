ef1389f313cf7e3ae97206e3e9d7e4cb3729fa1f
erenkucuk <erenkkucuk89@gmail.com>
2022-09-11 00:44:32 -1000

    added new lines

--- src/test/java/TestConnection.java
+++ src/test/java/TestConnection.java
@@ -21,35 +21,10 @@ public class TestConnection {
         try {
             Connection con = DriverManager.getConnection(url,username,password);
 
-            //this way creating statement object
-            //will allow us to move forward and backward easily when navigating through the result we got
-            Statement sttmnt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-
-            ResultSet rs = sttmnt.executeQuery("SELECT * FROM REGIONS");
-
-            //IN ORDER TO ACCESS RESULTSET, WE NEED TO KNOW WHICH ROW WE ARE
-            //BEFORE ENTERING THE FIRST ROW, THE CURSOR OF THE RESULTSET IS AT THE LOCATION KNOWN AS --BEFORE FIRST
-            //IN ORDER TO MOVE TO NEXT LINE WE NEED TO CALL next method of resultset
-            //IN ORDER TO GET ANY COLUMN DATA WE NEED TO CALL method getString or getDouble
-            rs.next();
-            //now we are at the first row
-            System.out.println("FIRST ROW REGION_NAME IS "+rs.getString("REGION_NAME"));
-
-            rs.next();
-            System.out.println("SECOND ROW REGION_NAME IS "+rs.getString("REGION_NAME"));
-
-            rs.next();
-            System.out.println("THIRD ROW REGION_NAME IS "+rs.getString("REGION_NAME"));
-
-            rs.next();
-            System.out.println("FOURTH ROW REGION_NAME IS "+rs.getString("REGION_NAME"));
-
-            //rs.next();
-            //BELOW LINE WILL NOT WORK. THERE IS NO ROW
-            //System.out.println("FIFTH ROW REGION_NAME IS "+rs.getString("REGION_NAME"));
-
-
             System.out.println("Connection was successfull");
+
+            //we close the connection
+            con.close();
 
 
         }catch (SQLException e){
