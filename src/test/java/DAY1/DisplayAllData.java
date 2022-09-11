dd2479c340809ab7eb33614271bd41fe84a1098d
erenkucuk <erenkkucuk89@gmail.com>
2022-09-11 00:39:16 -1000

    added new lines

--- src/test/java/DAY1/DisplayAllData.java
+++ src/test/java/DAY1/DisplayAllData.java
@@ -12,7 +12,7 @@ public class DisplayAllData {
 
         Connection con = DriverManager.getConnection(url, username, password);
         Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        ResultSet rs = stm.executeQuery("SELECT * FROM COUNTRIES");
+        ResultSet rs = stm.executeQuery("SELECT * FROM EMPLOYEES");
 
         rs.last();// how to go to last row
         int rowNum = rs.getRow();// how to get current row number
@@ -20,15 +20,31 @@ public class DisplayAllData {
 
         ResultSetMetaData rsmd = rs.getMetaData();
         int colCount = rsmd.getColumnCount();
-
+/*
         //print out entire first row in one line
         rs.first();
         //System.out.print(rs.getString(1));
         //System.out.print("\t" + rs.getString(2));
         //System.out.println("\t" + rs.getString(3));
         for (int col =1; col <= colCount; col++){
-            System.out.print(rs.getString(col)+" ");
+            System.out.print(rs.getString(col)+"\t");
         }
 
+ */
+
+        for (int col=1; col<=colCount; col++){
+            System.out.print(rsmd.getColumnName(col)+"\t");
+        }
+        System.out.println();
+
+        // i want to print every single row
+        // i am at the last row----- check line 17
+        rs.beforeFirst();
+        while(rs.next()){
+            for (int col =1; col <= colCount; col++){
+                System.out.print(rs.getString(col)+"\t");
+            }
+            System.out.println();
+        }
     }
 }
