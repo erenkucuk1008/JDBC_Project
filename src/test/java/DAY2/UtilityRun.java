f7bcc6dcc6876f4876acddb1648a01e114d4a576
erenkucuk <erenkkucuk89@gmail.com>
2022-09-11 03:05:10 -1000

    added new lines

--- src/test/java/DAY2/UtilityRun.java
+++ src/test/java/DAY2/UtilityRun.java
@@ -3,17 +3,25 @@ package DAY2;
 import utility.ConfigurationReader;
 import utility.DB_Utility;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UtilityRun {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         DB_Utility.createConnection();
 
 
-        String url = ConfigurationReader.getProperty("bookit.database.url");
-        String username = ConfigurationReader.getProperty("bookit.database.username");
-        String password = ConfigurationReader.getProperty("bookit.database.password");
+        String url = ConfigurationReader.getProperty("hr.database.url");
+        String username = ConfigurationReader.getProperty("hr.database.username");
+        String password = ConfigurationReader.getProperty("hr.database.password");
 
         DB_Utility.createConnection(url, username, password);
+
+        ResultSet result = DB_Utility.runQuery("SELECT * FROM REGIONS");
+        result.next();
+        System.out.println("REGION_NAME= " + result.getString("REGION_NAME"));
+
     }
 }
--- src/test/java/utility/DB_Utility.java
+++ src/test/java/utility/DB_Utility.java
@@ -1,10 +1,12 @@
 package utility;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DB_Utility {
+
+    private static Connection con;
+    private static Statement stm;
+    private static ResultSet rs;
 
     public static void createConnection() {
 
@@ -13,7 +15,7 @@ public class DB_Utility {
         String password = ConfigurationReader.getProperty("hr.database.password");
 
         try {
-            Connection con = DriverManager.getConnection(url,username,password);
+            con = DriverManager.getConnection(url,username,password);
             System.out.println("Connection was successfull");
         }catch (SQLException e){
             System.out.println("Connection has failed "+ e.getMessage());
@@ -22,10 +24,22 @@ public class DB_Utility {
 
     public static void createConnection(String url, String username, String password){
         try {
-            Connection con = DriverManager.getConnection(url,username,password);
+            con = DriverManager.getConnection(url,username,password);
             System.out.println("Connection was successfull");
         }catch (SQLException e){
             System.out.println("Connection has failed "+ e.getMessage());
         }
+    }
+
+    public static ResultSet runQuery(String sql){
+
+        try {
+            stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs = stm.executeQuery(sql);
+        } catch (SQLException e){
+            System.out.println("Error Occured While Running Query "+e.getMessage());
+        }
+
+        return rs;
     }
 }
